74e5ca9a2c5412b867b52bffc537a9599493df97
Nick.Wu <nick.wu@infortrend.com>
2023-09-08 18:03:33 +0800

    -

---
+++ src/App.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect } from 'react';
+import data from './data'; // 引入資料
+import './style.css'; // 引入CSS文件
+
+function App() {
+  // 定義狀態變數
+  const [selectedAttr, setSelectedAttr] = useState('暗'); // 選擇的屬性
+  const [selectedImageId, setSelectedImageId] = useState(null); // 選擇的圖片ID
+  const [selectedOption, setSelectedOption] = useState('Cross-Shaped_2');
+  const [filteredImages, setFilteredImages] = useState([]); // 過濾後的圖片列表
+
+  // 使用效果鉤子來過濾圖片列表
+  useEffect(() => {
+    // 根據選擇的屬性過濾圖片資料
+    const filteredData = data.filter((item) => item.attr === selectedAttr);
+
+    // 如果當前的選擇屬性分頁下有圖片，則選擇該分頁下的第一張圖片
+    if (filteredData.length > 0) {
+      setSelectedImageId(filteredData[0].id);
+    } else {
+      setSelectedImageId(null); // 否則清除選擇的圖片ID
+    }
+
+    const imageElements = filteredData.map((item) => (
+      <div key={item.id} className="image-wrapper">
+        <img
+          src={getThumbnailUrl(item.id)}
+          alt={`圖片 ${item.id}`}
+          width="50"
+          className={selectedImageId === item.id ? 'selected' : ''}
+          onClick={() => handleImageClick(item.id)}
+        />
+      </div>
+    ));
+    setFilteredImages(imageElements);
+  }, [selectedAttr, selectedImageId, selectedOption]);
+
+  // 處理屬性選擇的函數
+  const handleAttrChange = (attr) => {
+    setSelectedAttr(attr);
+  };
+
+  // 處理圖片點擊的函數
+  const handleImageClick = (id) => {
+    setSelectedImageId(id);
+  };
+
+  // 處理選項改變的函數
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  // 獲取縮圖的URL
+  const getThumbnailUrl = (id) => {
+    return `https://hiteku.github.io/img/tos/cards/icon/${id}i.png`;
+  };
+
+  // 獲取大圖的URL
+  const getImageUrl = (id) => {
+    var add = ''
+    if (id === 2828) add = '/1st'
+    return `https://hiteku.github.io/img/tos/number/${id}` + add + `/${selectedOption}.jpg`;
+  };
+
+  // 使用效果鉤子來過濾圖片列表
+  useEffect(() => {
+    // 根據選擇的屬性過濾圖片資料
+    const filteredData = data.filter((item) => item.attr === selectedAttr);
+
+    // 如果當前的選擇屬性分頁下有圖片，則選擇該分頁下的第一張圖片
+    if (filteredData.length > 0) {
+      // 檢查選擇的圖片是否存在於過濾後的列表中，如果不存在，選擇第一張圖片
+      const newSelectedImageId = filteredData.some((item) => item.id === selectedImageId)
+        ? selectedImageId
+        : filteredData[0].id;
+
+      setSelectedImageId(newSelectedImageId);
+    } else {
+      setSelectedImageId(null); // 否則清除選擇的圖片ID
+    }
+
+    const imageElements = filteredData.map((item) => (
+      <div key={item.id} className="image-wrapper">
+        <img
+          src={getThumbnailUrl(item.id)}
+          alt={`${item.id}`}
+          width="50"
+          className={selectedImageId === item.id ? 'selected' : ''}
+          onClick={() => handleImageClick(item.id)}
+        />
+      </div>
+    ));
+    setFilteredImages(imageElements);
+  }, [selectedAttr, selectedImageId, selectedOption]);
+
+  // 獲取不重複的屬性標籤
+  const attributeLabels = Array.from(new Set(data.map((item) => item.attr)));
+
+  return (
+    <div className="App">
+      <header>
+        <h1>固版轉法</h1>
+      </header>
+      <div className="container">
+        <div className="sidebar">
+          <ul>
+            {/* 渲染屬性標籤 */}
+            {attributeLabels.map((attr) => (
+              <li
+                key={attr}
+                className={selectedAttr === attr ? 'active' : ''}
+                onClick={() => handleAttrChange(attr)}
+              >
+                {attr}
+              </li>
+            ))}
+          </ul>
+          <div className="thumbnail-images">
+            {selectedAttr && (
+              <div className="thumbnail-images">
+                {filteredImages.length > 0 ? (
+                  filteredImages
+                ) : (
+                  <p>無相關圖片 {selectedAttr}</p>
+                )}
+              </div>
+            )}
+          </div>
+          <div className="option-select">
+            <label htmlFor="options">轉法 </label>
+            <select
+              id="options"
+              value={selectedOption}
+              onChange={(e) => handleOptionChange(e.target.value)}
+              className="styled-select" // 添加樣式
+            >
+              {[...Array(13).keys()].map((i) => (
+                <option key={i} value={i + 15}>
+                  {i + 15}
+                </option>
+              ))}
+              <option value="Cross-Shaped_2">雙十字</option>
+              <option value="Cross-Shaped_3">三十字</option>
+            </select>
+          </div>
+        </div>
+        <div className="content">
+          {selectedImageId && (
+            <div className="selected-image">
+              {/* 顯示所選圖片 */}
+              <img
+                src={getImageUrl(selectedImageId)}
+                alt={`目前尚未有${selectedImageId}轉法`}
+                style={{ width: '200px' }}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+// data.js
+const data = [
+  { id: 0, attr: '水' },
+  { id: 1, attr: '火' },
+  { id: 2, attr: '木' },
+  { id: 2726, attr: '光' },
+  { id: 2828, attr: '火' },
+  { id: 2909, attr: '光' },
+  { id: 10268, attr: '光' },
+  { id: 10336, attr: '火' },
+  { id: 10345, attr: '光' },
+  { id: 10370, attr: '暗' },
+  { id: 10388, attr: '暗' },
+  { id: 10410, attr: '光' },
+];
+
+export default data;
